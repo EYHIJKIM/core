4e05f8f9f5cb45311247b53aad0a1f4f86528993
kimze <crk33333@gmail.com>
2023-03-20 16:21:14 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,17 +10,24 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 //객체의 생성과 연결을 담당하는 클래스 >> DI의 완성.
+//어플리케이션 전체 구성을 한 눈에 볼 수 있음.
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 
 
 
