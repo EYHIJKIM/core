7081590208d8eb96d36c86bb9d96d26937c1888f
kimze <crk33333@gmail.com>
2023-04-01 15:15:13 +0900

    순수자바코드 스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //객체의 생성과 연결을 담당하는 클래스 >> DI의 완성.
 //어플리케이션 전체 구성을 한 눈에 볼 수 있음.
+//의존관계를 주입해줌.
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
+    public MemberRepository memberRepository() {
 
-    private MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    public OrderService orderService(){
+    @Bean
+    public   OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
+
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
         //회원 도메인 실행과 테스트
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+
+        //순수 자바 말고 스프링을 이용하도록 전환
+        //1. 스프링 컨테이너를 이용 >> 클래스 파라미터로 던져서 가져옴
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //2.컨텍스트에 기본적으로 메소드 이름으로 이름이 등록됨. 따라서 불러오고자 하는 Bean 메소드 이름을 파라미터로 넘김
+        MemberService memberService = applicationContext.getBean("memberService" ,MemberService.class);
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
