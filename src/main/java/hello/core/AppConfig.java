34df63233c229a175a2acdd175dff80fa3512169
kimze <crk33333@gmail.com>
2023-04-16 16:39:12 +0900

    싱글톤 방식의 주의점

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -18,18 +18,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+    //memberService / orderService 각 메소드 실행시에 같은 객체 "MemoryMemberRepository" return함. 싱글톤이 깨지는 것 처럼 보임. 스프링 컨테이너는 어떻게 해결할까?
+
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public   OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
 
     }
